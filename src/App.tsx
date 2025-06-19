3cbd2fd894bd322300e90b593051e818946441ca
Shirley Ding <122686936+pearjuicee@users.noreply.github.com>
2025-06-18 23:36:02 -0400

    add basic elements and segmenter
    - input area
    - token display and selection
    - definition area

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { SentenceInput } from './components/SentenceInput'
 
 function App() {
   return (
     <>
       <p>hello world Glossa!</p>
+      <SentenceInput />
     </>
   )
 }
